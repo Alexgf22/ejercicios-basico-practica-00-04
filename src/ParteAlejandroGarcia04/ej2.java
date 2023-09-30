c4c251261112c3a1616912fead89e049b205f70e
Alexgf22 <agarcia@iesrafaelalberti.es>
2023-09-30 16:39:46 +0200

    Ejercicios 2,4 y 6 terminados

--- src/ParteAlejandroGarcia04/ej2.java
+++ src/ParteAlejandroGarcia04/ej2.java
@@ -1,4 +1,26 @@
 package ParteAlejandroGarcia04;
 
+import java.util.Random;
+
 public class ej2 {
+
+    public static String obtenerCarta() {
+        String[] palos = {"picas", "corazones", "diamantes", "tréboles"};
+        String[] cartas = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+
+        Random random = new Random();
+        int indiceAleatorio1 = random.nextInt(palos.length);
+        String paloAleatorio = palos[indiceAleatorio1];
+
+        int indiceAleatorio2 = random.nextInt(cartas.length);
+        String cartaAleatoria = cartas[indiceAleatorio2];
+
+        return "La carta aleatoria es: " + cartaAleatoria + " de " + paloAleatorio;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(obtenerCarta() );
+    }
+
 }
--- src/ParteAlejandroGarcia04/ej4.java
+++ src/ParteAlejandroGarcia04/ej4.java
@@ -1,4 +1,29 @@
 package ParteAlejandroGarcia04;
 
+import java.util.Random;
+
 public class ej4 {
+
+    public static String obtenerNums() {
+        String[] numeros = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+
+
+        Random random = new Random();
+        StringBuilder resultado = new StringBuilder();
+        int i = 0;
+        while(i <= 20) {
+            int indiceAleatorio = random.nextInt(numeros.length);
+            String numAleatorio = numeros[indiceAleatorio];
+            resultado.append(numAleatorio).append(" ");
+            i++;
+        }
+
+        return resultado.toString().trim();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(obtenerNums() );
+    }
+
 }
